15a253509417b4a21a11d5ed63c7dba40250563d
Lation <Lation@Lation-PC.fritz.box>
2018-10-04 23:28:47 +0200

    implemented PokemonList, which reads the pokemonvalues.csv

--- Pokemon/src/pokemon/entities/Pokemon.java
+++ Pokemon/src/pokemon/entities/Pokemon.java
@@ -8,7 +8,7 @@ import pokemon.util.NatureModifier;
 
 public class Pokemon {
 
-    private enum Type {
+    public enum Type {
 	Normal, Fighting, Flying, Poison, Ground, Rock, Bug, Ghost, Steel, Fire, Water, Grass, Electric, Psychic, Ice, Dragon, Dark
     }
 
@@ -26,70 +26,121 @@ public class Pokemon {
 	Timid, Hasty, Jolly, Naive, Serious;
     }
 
-    private enum Gender {
-	M, W
+    public enum Ability {
+	Air_Lock, Arena_Trap, Battle_Armor, Blaze, Chlorophyll, Clear_Body, Cloud_Nine, Color_Change, Compound_Eyes, Cute_Charm, Damp, //
+	Drizzle, Drought, Early_Bird, Effect_Spore, Flame_Body, Flash_Fire, Forecast, Guts, Huge_Power, Hustle, Hyper_Cutter, Illuminate, //
+	Immunity, Inner_Focus, Insomnia, Intimidate, Keen_Eye, Levitate, Lightning_Rod, Limber, Liquid_Ooze, Magma_Armor, Magnet_Pull, //
+	Marvel_Scale, Minus, Natural_Cure, Oblivious, Overgrow, Own_Tempo, Pickup, Plus, Poison_Point, Pressure, Pure_Power, Rain_Dish, //
+	Rock_Head, Rough_Skin, Run_Away, Sand_Stream, Sand_Veil, Serene_Grace, Shadow_Tag, Shed_Skin, Shell_Armor, Shield_Dust, Soundproof, //
+	Speed_Boost, Static, Stench, Sticky_Hold, Sturdy, Suction_Cups, Swarm, Swift_Swim, Synchronize, Thick_Fat, Torrent, Trace, Truant, //
+	Vital_Spirit, Volt_Absorb, Water_Absorb, Water_Veil, White_Smoke, Wonder_Guard, Adaptability, Aftermath, Anger_Point, Anticipation, //
+	Bad_Dreams, Download, Dry_Skin, Filter, Flower_Gift, Forewarn, Frisk, Gluttony, Heatproof, Honey_Gather, Hydration, Ice_Body, //
+	Iron_Fist, Klutz, Leaf_Guard, Magic_Guard, Mold_Breaker, Motor_Drive, Multitype, No_Guard, Normalize, Poison_Heal, Quick_Feet, //
+	Reckless, Rivalry, Scrappy, Simple, Skill_Link, Slow_Start, Sniper, Snow_Cloak, Snow_Warning, Solar_Power, Solid_Rock, Stall, //
+	Steadfast, Storm_Drain, Super_Luck, Tangled_Feet, Technician, Tinted_Lens, Unaware, Unburden
+    }
+
+    public enum EggGroup {
+	Monster, Water_1, Water_2, Water_3, Bug, Flying, Field, Fairy, Grass, Undiscovered, Human_Like, Mineral, Amorphous, Ditto, Dragon
+    }
+
+    public enum LevelingRate {
+	Slow, Medium, Fast, MediumSlow, SlowThenFast, FastThenSlow
+    }
+
+    public enum Gender {
+	M, W, N
     }
 
     // util
     Random random = new Random();
+    private static int idCounter = 0;
 
     // specific stats
-    private int id;
-    private String name;
-    private String nickname;
-    private int level = 1;
-    private int currentHitpoints;
-    private Type type1;
-    private Type type2;
-    private Nature nature;
-    private Gender gender;
+    public int hiddenId;
+    public int id;
+    public String name;
+    public String nickname;
+    public int level = 1;
+    public int currentHitpoints;
+    public Nature nature;
+    public Type type1;
+    public Type type2;
+    public Ability ability;
+    public EggGroup eggGroup1;
+    public EggGroup eggGroup2;
+    public Gender gender;
+    public int captureRate;
+    public LevelingRate levelingRate;
+    public int expYield;
+    public int eggSteps;
+    public float height;
+    public float weight;
+    public boolean legendary;
 
     // calculated stats
-    private int maxhitpoints;
-    private int attack;
-    private int defense;
-    private int specialAttack;
-    private int specialDefense;
-    private int speed;
+    public int maxhitpoints;
+    public int attack;
+    public int defense;
+    public int specialAttack;
+    public int specialDefense;
+    public int speed;
 
     // base stats
-    private int baseHitpoints;
-    private int baseAttack;
-    private int baseDefense;
-    private int baseSpecialAttack;
-    private int baseSpecialDefense;
-    private int baseSpeed;
+    public int baseHitpoints;
+    public int baseAttack;
+    public int baseDefense;
+    public int baseSpecialAttack;
+    public int baseSpecialDefense;
+    public int baseSpeed;
 
     // iv's
-    private int individualHitpoints = random.nextInt(32);
-    private int individualAttack = random.nextInt(32);
-    private int individualDefense = random.nextInt(32);
-    private int individualSpecialAttack = random.nextInt(32);
-    private int individualSpecialDefense = random.nextInt(32);
-    private int individualSpeed = random.nextInt(32);
+    public int individualHitpoints = random.nextInt(32);
+    public int individualAttack = random.nextInt(32);
+    public int individualDefense = random.nextInt(32);
+    public int individualSpecialAttack = random.nextInt(32);
+    public int individualSpecialDefense = random.nextInt(32);
+    public int individualSpeed = random.nextInt(32);
+
+    // iv yield
+    public int hitpointsYield;
+    public int attackYield;
+    public int defenseYield;
+    public int specialAttackYield;
+    public int specialDefenseYield;
+    public int speedYield;
 
     // ev's
-    private int effortHitpoints;
-    private int effortAttack;
-    private int effortDefense;
-    private int effortSpecialAttack;
-    private int effortSpecialDefense;
-    private int effortSpeed;
+    public int effortHitpoints = 0;
+    public int effortAttack = 0;
+    public int effortDefense = 0;
+    public int effortSpecialAttack = 0;
+    public int effortSpecialDefense = 0;
+    public int effortSpeed = 0;
 
-    public Pokemon(int level) {
-	setNature();
-	calculateMaximumStats();
-
-	currentHitpoints = maxhitpoints;
+    public Pokemon() {
+	hiddenId = idCounter;
+	idCounter++;
     }
 
-    public static Pokemon create() {
+    public Pokemon create(int id, int level) {
+	Pokemon pokemon = new Pokemon();
+	this.level = level;
+	setNature();
+	setGender();
+	calculateMaximumStats();
+	currentHitpoints = maxhitpoints;
+
 	return null;
     }
 
     private void setNature() {
 	List<Nature> natures = Arrays.asList(Nature.values());
 	this.nature = natures.get(random.nextInt(25));
+    }
+
+    private void setGender() {
+
     }
 
     private void calculateMaximumStats() {
---
+++ Pokemon/src/pokemon/entities/PokemonList.java
@@ -0,0 +1,83 @@
+package pokemon.entities;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import pokemon.entities.Pokemon.Ability;
+import pokemon.entities.Pokemon.EggGroup;
+import pokemon.entities.Pokemon.Gender;
+import pokemon.entities.Pokemon.LevelingRate;
+import pokemon.entities.Pokemon.Type;
+
+public class PokemonList {
+
+    Random random = new Random();
+    private static Map<Integer, Pokemon> pokemonMap = new HashMap<>();
+
+    public void initPokemon() {
+	try (BufferedReader br = new BufferedReader(new FileReader("pokemonvalues.csv"))) {
+	    br.readLine();
+	    Pokemon pokemon = null;
+	    String line;
+
+	    while ((line = br.readLine()) != null) {
+		pokemon = new Pokemon();
+		String[] parameters = line.split(";");
+		pokemon.id = Integer.parseInt(parameters[0]);
+		pokemon.name = parameters[1];
+		pokemon.type1 = Type.valueOf(parameters[2]);
+		pokemon.type2 = Type.valueOf(parameters[3]);
+		if (parameters[4].split(",").length == 1)
+		    pokemon.ability = Ability.valueOf(parameters[4].split(",")[0]);
+		else
+		    pokemon.ability = Ability.valueOf(parameters[4].split(",")[random.nextInt(2)]);
+		if (parameters[5].split(",").length == 1)
+		    pokemon.eggGroup1 = EggGroup.valueOf(parameters[5]);
+		else {
+		    pokemon.eggGroup1 = EggGroup.valueOf(parameters[5].split(",")[0]);
+		    pokemon.eggGroup2 = EggGroup.valueOf(parameters[5].split(",")[1]);
+		}
+		if (parameters[6].equals("100"))
+		    pokemon.gender = Gender.M;
+		else if (parameters[6].equals("0"))
+		    pokemon.gender = Gender.W;
+		else if (parameters[6].equals("-"))
+		    pokemon.gender = Gender.N;
+		else if (random.nextInt(100) < Integer.parseInt(parameters[6]))
+		    pokemon.gender = Gender.M;
+		else
+		    pokemon.gender = Gender.W;
+		pokemon.captureRate = Integer.parseInt(parameters[7]);
+		pokemon.levelingRate = LevelingRate.valueOf(parameters[8]);
+		pokemon.expYield = Integer.parseInt(parameters[9]);
+		pokemon.eggSteps = Integer.parseInt(parameters[10]);
+		pokemon.height = Float.parseFloat(parameters[11]);
+		pokemon.weight = Float.parseFloat(parameters[12]);
+		if (parameters[13].equals("0"))
+		    pokemon.legendary = false;
+		else
+		    pokemon.legendary = true;
+		pokemon.baseHitpoints = Integer.parseInt(parameters[14]);
+		pokemon.baseAttack = Integer.parseInt(parameters[15]);
+		pokemon.baseDefense = Integer.parseInt(parameters[16]);
+		pokemon.baseSpecialAttack = Integer.parseInt(parameters[17]);
+		pokemon.baseSpecialDefense = Integer.parseInt(parameters[18]);
+		pokemon.baseSpeed = Integer.parseInt(parameters[19]);
+		pokemon.hitpointsYield = Integer.parseInt(parameters[20]);
+		pokemon.attackYield = Integer.parseInt(parameters[21]);
+		pokemon.defenseYield = Integer.parseInt(parameters[22]);
+		pokemon.specialAttackYield = Integer.parseInt(parameters[23]);
+		pokemon.specialDefenseYield = Integer.parseInt(parameters[24]);
+		pokemon.speedYield = Integer.parseInt(parameters[25]);
+	    }
+	    pokemonMap.put(pokemon.id, pokemon);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+}
