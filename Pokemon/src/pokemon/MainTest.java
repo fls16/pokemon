abd6b45066840dd7debd720a35e00a6db0c6f7f5
Lation <Lation@Lation-PC.fritz.box>
2018-10-07 13:08:14 +0200

    just a test

--- Pokemon/src/pokemon/MainTest.java
+++ Pokemon/src/pokemon/MainTest.java
@@ -19,6 +19,22 @@ public class MainTest {
 	for (Pokemon p : pokemonList) {
 	    System.out.println(p.toString());
 	}
+
+	Pokemon pokemon = Pokemon.create(PokemonTemplate.pokemonMap.get(493), 100);
+	pokemon.effortHitpoints = 255;
+	pokemon.effortAttack = 255;
+	pokemon.effortDefense = 255;
+	pokemon.effortSpecialAttack = 255;
+	pokemon.effortSpecialDefense = 255;
+	pokemon.effortSpeed = 255;
+	pokemon.individualHitpoints = 31;
+	pokemon.individualAttack = 31;
+	pokemon.individualDefense = 31;
+	pokemon.individualSpecialAttack = 31;
+	pokemon.individualSpecialDefense = 31;
+	pokemon.individualSpeed = 31;
+	pokemon.calculateMaximumStats();
+	System.out.println(pokemon.toString());
     }
 
 }
